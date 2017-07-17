00ef91627d6e4a68a9f4adb92400b5f21a177920
MattBixley <mattbixley72@gmail.com>
2017-07-18 09:22:53 +1200

    themes

--- polls.R
+++ polls.R
@@ -1,12 +1,9 @@
+library(tidyverse)
 library(ggplot2)
-library(dplyr)
-library(tidyr)
 library(lubridate)
 library(xkcd)
 library(ggthemes)
 library(scales)
-
-
 
 poll <- read.table(file="polls.csv",header=T,sep=",")
 tail(poll)
@@ -20,20 +17,6 @@ colnames(pollplot) <- c("Order","Date","Party","Poll")
 pollplot$Date <- as.Date(pollplot$Date,"%Y%B%d")
 head(pollplot)
 
-a <- ggplot(data=pollplot,aes(x=Date,y=Poll,colour=Party)) + 
-  stat_smooth(formula = y ~ poly(x,5), method="glm", size=1,aes(weight=Date)) + 
-  scale_y_continuous(name="Percent of the Vote",breaks=seq(0,60,5)) +
-  scale_color_manual(values=c("brown","blue", "black")) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 0.6)) +
-  geom_point(position = position_jitter(width=0.1, height=0.1)) +
-  labs( x = "Polling Date",title ="Going to The Polls",
-      subtitle = "Guessing the Election",
-      caption = "Who does Winston 1st Choose")
-a + theme_xkcd()
-
-mm <- glm(Poll~Date,data=pollplot,family=binomial)
-
-## with winston
 pollW <- poll %>% select(Order, Year, Month, National, Labour, Green, NZFirst) %>% 
   mutate(GLW=Labour + Green + NZFirst, NW = National + NZFirst, Date=paste0(Year,Month,"15")) %>% 
   select(Order,Date,NW,GLW) %>% gather(key=Order,Date) 
@@ -43,6 +26,20 @@ colnames(pollW) <- c("Order","Date","Party","Poll")
 pollW$Date <- as.Date(pollW$Date,"%Y%B%d")
 head(pollW)
 
+# GL + N + W
+a <- ggplot(data=pollplot,aes(x=Date,y=Poll,colour=Party)) + 
+  stat_smooth(formula = y ~ poly(x,5), method="glm", size=1,aes(weight=Date)) + 
+  scale_y_continuous(name="Percent of the Vote",breaks=seq(0,60,5)) +
+  scale_color_manual(values=c("brown","blue", "black")) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 0.6)) +
+  geom_point(position = position_jitter(width=0.1, height=0.1)) +
+  labs( x = "Polling Date",title ="Going to The Polls",
+      subtitle = "Guessing the Election",
+      caption = "Who does Winston 1st Choose")
+#a + theme_xkcd()
+a
+
+## with winston
 w <- ggplot(data=pollW,aes(x=Date,y=Poll,colour=Party)) + 
   stat_smooth(formula = y ~ poly(x,5), method="glm", size=1,aes(weight=Date)) + 
   scale_y_continuous(name="Percent of the Vote",breaks=seq(0,60,5)) +
@@ -53,12 +50,23 @@ w <- ggplot(data=pollW,aes(x=Date,y=Poll,colour=Party)) +
         subtitle = "Guessing the Election",
         caption = "Who does Winston 1st Choose")
 
-w + theme_economist() + scale_colour_economist() + scale_y_continuous(position = "right")
-w + theme_excel() + scale_colour_excel()
-w + theme_calc() + scale_color_calc()
+ww <- ggplot(data=pollW,aes(x=Date,y=Poll,colour=Party)) + 
+  stat_smooth(formula = y ~ poly(x,5), method="glm", size=1,aes(weight=Date)) + 
+  scale_color_manual(values=c("brown","blue")) +
+  geom_point(position = position_jitter(width=0.1, height=0.1))
 
-w + scale_color_fivethirtyeight("cyl") + theme_fivethirtyeight()
-w + theme_few() + scale_colour_few()
-w + theme_wsj() + scale_colour_wsj("colors6", "")
-w + theme_gdocs() + scale_color_gdocs()
-w + theme_tufte() 
+ww
+
+ww + theme(plot.subtitle = element_text(size = 12), 
+    plot.caption = element_text(size = 12), 
+    axis.ticks = element_line(colour = "gray15", 
+        size = 0.9), axis.title = element_text(family = "serif", 
+        size = 20, face = "bold.italic"), 
+    axis.text = element_text(family = "serif", 
+        size = 15, colour = "gray4", angle = 30), 
+    plot.title = element_text(family = "serif", 
+        size = 40, face = "bold", hjust = 0.5), 
+    plot.background = element_rect(linetype = "dotdash"), 
+    legend.direction = "horizontal") +labs(title = "The Polls", y = "Percent", 
+    subtitle = "Winston will choose what Winston Chooses", 
+    caption = "What could be worse? - The Trump")
