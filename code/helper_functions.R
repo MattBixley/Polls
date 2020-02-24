ce624f86a7f1a01b076ea605fc7bda78a5b3e864
MattBixley <mattbixley72@gmail.com>
2020-02-24 14:17:12 +1300

    bring on the purrr

--- code/helper_functions.R
+++ code/helper_functions.R
@@ -14,3 +14,14 @@ check_package <- function(x){
 # usage
 # check_package(c("package1", "package2"))
 
+# return last or nth to last value of a list
+get_last <- function(x, n){
+    x %>% map(str_split, ' ') %>% # split the string
+    map(1) %>% # unnest the string
+    map_dfr(~ tibble(last = .x[[length(.x) - n + 1]])) # grab the last nth value
+}
+
+# usage
+# my_list <- list(one = "1 sep 2014", two = "1-2 sep 2015", three = "1 - 2 sept 2016")
+# get_last(my_list, 1) # gets the year
+# get_last(my_list, 2) # gets the month
